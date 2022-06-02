6f75102c8056db39f91eb9eec7c0339d1c6cd131
Chris Parker <cwbparker@gmail.com>
2022-06-02 12:22:34 -0700

    Add react router and setup page routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Dashboard from "./pages/Dashboard";
+import TicketPage from "./pages/TicketPage";
 
 const App = () => {
   return (
     <div className="App">
-     
+     <BrowserRouter>
+     <Nav/>
+      <Routes>
+        <Route path='/' element={<Dashboard />}/>
+        <Route path='/ticket' element={<TicketPage />}/>
+        <Route path='/ticket/:id' element={<TicketPage editMode={true}/>}/>
+      </Routes>
+     </BrowserRouter>
     </div>
   )
 }
