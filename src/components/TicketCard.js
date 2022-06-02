275259b5c22b24fdde8cbe74475dc55c7fd2d825
Chris Parker <cwbparker@gmail.com>
2022-06-02 12:56:54 -0700

    Finish nav styling and begin work on ticket card and dashboard

--- src/components/TicketCard.js
+++ src/components/TicketCard.js
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom"
+import AvatarDisplay from "./AvatarDisplay"
+import StatusDisplay from "./StatusDisplay"
+import PriorityDisplay from "./PriorityDisplay"
+import ProgressDisplay from "./ProgressDisplay"
+import DeleteBlock from "./DeleteBlock"
+
+
 const TicketCard = () => {
     return (
-      <div>
-          TicketCard
+      <div classname="ticket-card">
+         
+         <Link>
+            <div className="ticket-color"></div>
+            <h3>title</h3>
+            <AvatarDisplay/>
+            <StatusDisplay/>
+            <PriorityDisplay/>
+            <ProgressDisplay/>
+         </Link>
+         <DeleteBlock/>
       </div>
     )
   }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,48 @@
 import TicketCard from "../components/TicketCard";
 
 const Dashboard = () => {
+    
+    const tickets = [
+        {
+            category: 'Q1 2022',
+            color: 'red',
+            title: 'NFT Safety 101 Video',
+            owner: 'Chris Parker',
+            avatar: '',
+            status: 'done',
+            priority: 5,
+            progress: 40,
+            description: 'Make a video showcasing how to work with NFTs Safely, including how to know if one is not genuine',
+            timestamp: '2022-02-11T07:36:17+0000'
+        },
+        {
+            category: 'Q1 2022',
+            color: 'red',
+            title: 'Build and Sell AI Model',
+            owner: 'Chris Parker',
+            avatar: '',
+            status: 'working on it',
+            priority: 2,
+            progress: 70,
+            description: 'Make a video about AI',
+            timestamp: '2022-02-13T07:36:17+0000'
+        },
+        {
+            category: 'Q2 2022',
+            color: 'blue',
+            title: 'Build a bot',
+            owner: 'Chris Parker',
+            avatar: '',
+            status: 'working on it',
+            priority: 3,
+            progress: 10,
+            description: 'Make a video about making a bot',
+            timestamp: '2022-02-15T07:36:17+0000'
+        },
+    ]
+    
+
+
     return (
       <div className="dashboard">
           <h1>My Projects</h1>
